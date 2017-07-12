593a416158e3050f1103d2eaf88f2209657e8171
hansol <gksthf1226@naver.com>
2017-07-12 20:03:49 +0900

    enhance sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,9 @@
 #include "sum.h"
 
 int sum(int n){
-	int res = 0;
-	for(int i=1;i<=n;i++){
-		res = res + i;
-	}
-	return res;
+	//int res = 0;
+	//for(int i=1;i<=n;i++){
+	//	res = res + i;
+	//}
+	return n+1 * n / 2;
 }
